67df8c0c8125a5615c99e299964056c142bcf121
syeda zehra <zehrasyeda@gmail.com>
2018-06-12 20:25:54 -0500

    almost there

---
+++ Trivia-game-2/assets/javascript/game.js
@@ -0,0 +1,142 @@
+$(document).ready(function(){
+    $("#startBtn").click(function(){
+        $(this).hide();
+        newGame();
+        
+    });
+    $("#startBtn").click(function(){
+        $(".card-header").hide();
+        newGame();
+        
+    });
+
+    $("#startOverBtn").click(function(){
+        $(this).hide();
+        newGame();
+
+    });
+    
+    function newGame(){
+        runTimer();
+        $('#finalMessage').empty();
+        $('#correctAnswers').empty();
+        $('#incorrectAnswers').empty();
+        currentQuestion = 0;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        newQuestion();
+    }
+
+    var finalMessage;
+    var correctAnswer;
+    var incorrectAnswer;
+    var currentQuestion; 
+    var seconds; 
+    var userSelect;
+
+    var time = 5;
+        var intervalID;
+        function runTimer(){
+            intervalID= setInterval(timer,1000);
+        }
+        function timer(){
+            $('.timer').html('<h5>Time Left: '+time+' secs</h5>');
+            time--;
+            if(time===-1 || userSelect== true){	
+                stopTimer();
+                incorrectAnswer++;
+                newQuestion();
+            }
+        }
+        function stopTimer(){
+            clearInterval(intervalID);
+        }
+    
+
+    function newQuestion(){
+        if(currentQuestion<triviaQuestions.length){
+            stopTimer();
+             time=5;
+             runTimer();
+             $('#message').empty();
+             $("#answerList").empty();
+             var question = triviaQuestions[currentQuestion];
+             var questionImage = $("<img>");
+             questionImage.attr('src', question.image);
+             questionImage.attr('class', 'question-img');
+             $(".image").html(questionImage);
+             $(".question").text(question.question);
+     
+             for(var i = 0;i<4; i++){
+                 var optionDiv = $('<div class="selection">');
+                 var option = $('<input type="radio" name="choice" value="new button" />');
+                 var optionLabel = $('<label>');
+                 optionLabel.text(question.answerList[i]);
+                 optionDiv.append(option);
+                 optionDiv.append(optionLabel)
+                 $("#answerList").append(optionDiv);
+     
+                 option.click(function(){
+                    stopTimer();
+                    console.log(option)
+                     if(question.answerList.indexOf(answerList) == question.answer){
+                         $("#message").text(messages.correct)
+                         correctAnswer++;
+                     }else{
+                         incorrectAnswer++;
+                         $("#message").text(messages.incorrect);
+                     }
+                     setTimeout(newQuestion, 2000);
+                     // newQuestion();
+                 });
+             }
+     
+             currentQuestion++;
+        } else {
+            scoreboard();
+            //alert('results\nCorrect: ' + correctAnswer + "\nIncorrect: " + incorrectAnswer);
+        }
+            // $(this).append(question.answerList[i]);
+            
+    }
+        var messages = {
+            correct: "Yes, that's right!",
+            incorrect: "No, that's not it.",
+            endTime: "Out of time!",
+            finished: "Alright! Let's see how well you did."
+        }
+    
+           
+        
+        function scoreboard(){
+            $('#timeLeft').empty();
+            $('#message').empty();
+            $('#correctedAnswer').empty();
+            $('#finalMessage').html(messages.finished);
+            $('#correctAnswers').html("Correct Answers: " + correctAnswer);
+            $('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+            $('#startOverBtn').addClass('reset');
+            $('#startOverBtn').show();
+            $('#startOverBtn').html('Start Over?');
+        }
+    });
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Trivia-game-2/assets/javascript/questions.js
@@ -0,0 +1,62 @@
+var triviaQuestions = [
+    {
+    image:"assets/images/Wright-bros-plane.jpg",
+    question: "In What year did the Wright Brothers make the first powered flight?",
+    answerList: ["1905", "1893", "1903", "1933"],
+    answer: 2
+},{
+    image: "assets/images/Qantas.jpg",
+    question: "Which airline this logo belongs to?",
+    answerList: ["Air New Zealand", "Jetstar", "Virgin Australia", "Qantas"],
+    answer: 3
+    
+},{
+    image:"assets/images/download.jfif",
+    question: "Which aircraft is Larger?",
+    answerList: ["Boeing 747", "Boeing 787", "Boieng 900", "Boeing 737"],
+    answer: 0
+},{
+    image:"assets/images/A380.jfif",
+    question: "What is the name of the largest Commercial aircraft which came out in 2006-2008?",
+    answerList: ["Airbus A900", "Airbus A380", "Airbus Double ", "Unknown to public"],
+    answer: 1
+},{
+    image: "assets/images/Hawaiaan.jpg",
+    question: "A picture is worth thousand words holds true for this one!",
+    answerList: ["Air Tahiti", "Polynesian Airlines", "Hawaiian Airlines", "LIAT"],
+    answer: 2
+    
+},{
+    image: "assets/images/lufthansa.jpg",
+    question: "Which airline this logo belongs to?",
+    answerList: ["Alitalia", "Air Berlin", "Air Asia", "Lufthansa"],
+    answer: 3
+},
+{
+    image: "assets/images/delta.jpg",
+    question: "Which US Airline uses this?",
+    answerList: ["United Airlines", "Delta-Airlines", "Southwest Airlines", "Spirit Airlines"],
+    answer: 1
+},{
+    image: "assets/images/emirates.jpg",
+    question: "Which middle Eastern airline uses this?",
+    answerList: ["Etihad", "Qatar Airways", "Emirates", "Air Arabia"],
+    answer: 2
+},{
+    image: "assets/images/cathaypacific.jpg",
+    question: "To which airline does this belong ?",
+    answerList: ["Cathay Pacific", "Air Asia", "Singapore Airlines", "Malaysian Airline"],
+    answer: 0
+},{
+    image: "assets/images/concorde.jpg",
+    question: "When was concord's last flight?",
+    answerList: ["Oct 24, 2003", "Sep 27,2003 ", "Nov 3,2003", "Jan 1st, 2004."],
+    answer: 0
+    
+},{
+    image: "assets/images/Iranair.jpg",
+    question: "Have you ever seen this before?",
+    answerList: ["Atlasjet", "Oman Air", "Iran Air", "Air India"],
+    answer: 2
+}
+];
