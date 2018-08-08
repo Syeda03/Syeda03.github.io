122f6463b76aefae9de7547b492bcbd49ec3699b
syeda zehra <zehrasyeda@gmail.com>
2018-08-07 19:08:38 -0500

    adding friendfinder

---
+++ friendfinder/app/data/friends.js
@@ -0,0 +1,20 @@
+var friends = [
+	{
+		name: 'The Introvert friend',
+		photo: 'url("../images/Introverts-hate-phone.jpg")',
+		scores: [5, 1, 2, 2, 5, 5, 5, 1, 3, 1]
+	},{
+		name: 'The Extrovert friend',
+		photo: 'url("../images/extrovert.webp")',
+		scores: [1, 5, 3, 4, 1, 1, 1, 5, 3, 5]
+	},{
+		name: 'The Narcissist friend',
+		photo: 'url("../images/narcissist.jpg")',
+		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	},{
+		name: 'The helpful friend',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjoiqLD2dvcAhWH_p8KHRZ2A_MQjRx6BAgBEAU&url=https%3A%2F%2Fwww.rd.com%2Fadvice%2Frelationships%2Fconversational-narcissism%2F&psig=AOvVaw15h2oPIE9dZRmt7GXzgXON&ust=1533756584430417',
+		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+	}];
+
+module.exports = friends;
